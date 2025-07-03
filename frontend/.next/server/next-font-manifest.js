3fd90fb9b42d627017c18c76609c284928dcb37b
Rex L <rex@not-rex.org>
2025-07-03 18:36:47 -0400

    stuff

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/rexliu/staff-mgmt-tool/frontend/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
